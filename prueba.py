58db47fc9a10f8ff9e2327d4a7f928c969275acd
felipe-lozano <jhonnyfelipe1807@gmail.com>
2024-09-11 16:44:45 -0500

    se agrego un archivo prueba2.py

--- prueba.py
+++ prueba.py
@@ -1,5 +1,11 @@
 import time
 print("hola mundo \n")
 print("segundo reglon")
-time.sleep(10)
-print("chao mundo")
+time.sleep(3)
+
+#print("chao mundo")
+print ("mentiras volvi")
+
+print (type("hola"))# esta funcion me dice que tipo de dato es lo que esta entre parentesis 
+print (type(6))
+print (type(5.5))
