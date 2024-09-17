bf7a149727837cffe1170ebcef4f31fe7a1167dd
felipe-lozano <jhonnyfelipe1807@gmail.com>
2024-09-17 17:32:47 -0500

    papel o tijeras

--- prueba.py
+++ prueba.py
@@ -10,3 +10,87 @@ print (type("hola"))# esta funcion me dice que tipo de dato es lo que esta entre
 print (type(6))
 print (type(5.5))
 print ("pruebagitignore")
+print ("--------------------------------------------------------")
+bool=True
+print (bool)
+print (type(bool))
+numero= 5
+texto= "hola"
+suma= str(numero) + texto
+print (suma)
+
+bool1=True
+bool2=False
+
+suma2= bool1+bool2
+print (suma2)
+A= False
+B= False
+C= False
+D= True
+
+Resultado= ((A and B)and C) or D
+print (Resultado)
+
+A= True
+B= True
+C= True
+D= True
+Resultado2= ((A and B)and C) or D
+print (Resultado2)
+
+def pitagoras (a,b):
+    global hipotenusa
+    hipotenusa = (a**2 + b**2)**0.5
+    return hipotenusa
+
+#pitagoras (3,4)
+#print ( hipotenusa)
+
+def suma_dos_valores (sumando1, sumando2):
+    global resultado
+    resultado= sumando1 + sumando2
+    return resultado 
+
+def despeje_x (): 
+    global x
+    b= int(input("ingrese ahora el valor de b= ")) 
+    c= int(input("ingrese ahora el valor de c= ") )
+    x = (c-b)/2
+    print ( x )
+    return x
+
+#despeje_x()
+false=0 
+true=1
+#A= bool(input ("ingrese el valor de= "))
+#B= bool(input ("ingrese el valor de= "))
+#C= bool(input ("ingrese el valor de= "))
+
+#Resultado2= (A and B)and C
+#print ("resultado= ",Resultado2)
+
+def pitagoras_funcion_sumar():
+    global resulpitagoras
+    a= int(input("ingrese ahora el valor de a= ")) 
+    b= int(input("ingrese ahora el valor de b= ") )
+
+    a2=a**2
+    b2=b**2
+
+    suma= suma_dos_valores (a2,b2)
+    resulpitagoras= suma**0.5
+    print ( "el valor de pitagoras es= ",resulpitagoras)
+    return resulpitagoras
+
+pitagoras_funcion_sumar ()
+
+def contador_de_letras (texto):
+    letras = texto.split ()
+    frecuencia ={}
+    for letras in letras:
+        if letras in frecuencia:
+            frecuencia[letras] += 1
+        else:
+            frecuencia[letras] = 1
+    return frecuencia
--- prueba2.py
+++ prueba2.py
@@ -1,1 +1,55 @@
-print("segundo documento")
+def suma_dos_valores (sumando1, sumando2):
+    global resultado
+    resultado= sumando1 + sumando2
+    return resultado 
+
+#suma_dos_valores (4,5)
+#print ("primera suma")
+#print (resultado )
+#suma_dos_valores (1,2)
+#print ("segunda suma")
+#print (resultado)
+
+def calculadores_dos_valores (numero1,numero2,operador):
+    global resultado1
+    if operador == "suma": #si el operador es 1 es suma
+        resultado1= numero1 + numero2
+        return resultado1
+    elif operador == "resta": #si el operador es 2 es resta
+        resultado1= numero1 - numero2
+        return resultado1
+    elif operador == "multiplicacion" :#si el operador es 2 es multiplicacion 
+        resultado1= numero1 * numero2
+        return resultado1
+    elif operador == "division": #si el operador es 2 es division 
+        if numero2 == 0:
+            return "Error: División por cero"
+        else:
+            resultado1= numero1 / numero2
+            return resultado1     
+    else:
+        print ("el numero ingresado no  es valido")
+        return resultado1
+
+calculadores_dos_valores(1,2,1)
+print ("suma", resultado1)
+
+
+calculadores_dos_valores(1,2,2)
+print ("resta", resultado1)
+
+calculadores_dos_valores(1,2,3)
+print ("multiplicacion", resultado1)
+
+calculadores_dos_valores(1,2,4) 
+print ("division", resultado1)
+
+
+def calculador_de_teoremas (n1,n2):
+    global resultado_final
+    resultado= (n1*n1) + (n2*n2) 
+    resultado_final= resultado * resultado
+    return resultado_final
+
+calculador_de_teoremas (4,5)
+print (resultado_final)
