1d3431c78f524edcdef11d9cafcb434daf262907
felipe-lozano <jhonnyfelipe1807@gmail.com>
2024-09-18 12:35:59 -0500

    papel o tijeras1

--- prueba.py
+++ prueba.py
@@ -94,3 +94,42 @@ def contador_de_letras (texto):
         else:
             frecuencia[letras] = 1
     return frecuencia
+
+
+import random
+print("¡Bienvenido al juego de piedra, papel o tijera!") 
+print("Elije tu opción:") 
+print ("1. Piedra") 
+print("2. Papel") 
+print("3. Tijera")
+ 
+# Generar la elección del ordenador 
+jugador = random.randint(1, 3)
+ 
+# Asignar valores numéricos a las opciones 
+if jugador == 1: 
+      opcion_computadora1 = "Piedra" 
+elif jugador == 2: 
+      opcion_computadora1 = "Papel" 
+else: 
+      opcion_computadora1 = "Tijera"
+
+
+# Generar la elección del ordenador 
+computadora = random.randint(1, 3)
+ 
+# Asignar valores numéricos a las opciones 
+if computadora == 1: 
+      opcion_computadora = "Piedra" 
+elif computadora == 2: 
+      opcion_computadora = "Papel" 
+else: 
+      opcion_computadora = "Tijera"
+
+# Determinar el ganador 
+if jugador == computadora: 
+     print("Empate. la computadora escogio= ", opcion_computadora , "y tu =", opcion_computadora1) 
+elif (jugador == 1 and computadora == 3) or (jugador == 2 and computadora == 1) or (jugador == 3 and computadora == 2): 
+     print("¡Ganaste! la computadora escogio= ", opcion_computadora , "y tu =", opcion_computadora1) 
+else: 
+     print("Perdiste. la computadora escogio= ", opcion_computadora , "y tu =", opcion_computadora1)
